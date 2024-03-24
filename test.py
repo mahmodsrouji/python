098765a8e22b28543ab59b8b4c4c3fb5aeb68b87
Mahmoud Srouji <mahmod-srouji@hotmail.com>
2024-03-24 21:25:21 +0300

    This is my second updated for test.py

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-#The project in only three lines!
+# The project in only three lines!
 import random 
-# print("Welcome to 'whose wallest?' \n You will give me a list of names, and I will pick a person to pay randomly")
-# print(f"Please ask '{random.choice(input("If you're ready, Enter the names separated by commas ").split(', '))} to take out his wallet. Dinner is on him! " )
+print("Welcome to 'whose wallest?' \n You will give me a list of names, and I will pick a person to pay randomly")
+print(f"Please ask '{random.choice(input("If you're ready, Enter the names separated by commas ").split(', '))} to take out his wallet. Dinner is on him! " )
 
 
-# name1 = input("If you're ready, Enter the names separated by commas ").split(', ')
-# print(name1)
+name1 = input("If you're ready, Enter the names separated by commas ").split(', ')
+print(name1)
 split = "test. test2. test 3".split(". ")
 print(split)
 names = ['1', '3', '5']
 output = random.choice(split)
 print(output)
 
-# print("The dinner is on " + random.choice(input(f"Welocme to 'whose wallest? \n You will give me a list of names, and I will pick a person to pay randomly \n Enter the names seperated by commas: ").split(', ')))
+print("The dinner is on " + random.choice(input(f"Welocme to 'whose wallest? \n You will give me a list of names, and I will pick a person to pay randomly \n Enter the names seperated by commas: ").split(', ')))
 
