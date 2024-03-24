38752b70b65e36ef0403bba3791f85bbb80c2c03
Mahmoud Srouji <mahmod-srouji@hotmail.com>
2024-03-24 19:24:12 +0300

    comment

---
+++ octucode.py
@@ -0,0 +1,8 @@
+course = "python"
+level = "Beginner"
+teacher = "ibrahim"
+user_name = input("what is your name?\n")
+print("Nice to meet you, " + user_name)
+print( len (user_name))
+user_PIN = input("What is your PIN number? \n")
+print(type (user_PIN))
